52b80e479ad1d17c364090a3c70f46c0c1cafc38
Sandeep Kesarkar <sandeep@rxnt.com>
2018-06-13 16:34:54 -0400

    Added username in mongoose

--- app/helpers/mongodb.js
+++ app/helpers/mongodb.js
@@ -1,11 +1,42 @@
 const globals = require('../globals');
 const config = require('../config')
 
-globals.mongoose.connect(config.database.mongodb.connection_string, err => {
-  if (err) {
-    console.log('Error while connecting to MongoDB.........');
-    throw err;
-  } else {
-    console.log('Connected to MongoDB!!!!!!!!');
-  }
+//MongoDB uri to connect
+const dburi = 'mongodb://' + config.database.mongodb.server + ':'
+                          + config.database.mongodb.port
+                          + '/' + config.database.mongodb.database;
+
+//MongoDB options to be used while connecting
+const dboptions = {
+                    user: config.database.mongodb.user,
+                    pass: config.database.mongodb.pass,
+                    auth: {
+                      authdb: config.database.mongodb.authdb
+                    }
+                  };
+
+//Make connection using mongoose
+globals.mongoose.connect(dburi, dboptions);
+
+//Log after mongoose connection has been made
+globals.mongoose.connection.on('connected', () => {
+  console.log('Mongoose connected to MongoDB on ' + config.database.mongodb.server);
+});
+
+//Log error if mongoose has any error
+globals.mongoose.connection.on('error', (err) => {
+  console.log('Mongoose conection errror ' + err);
+});
+
+//Log after mongoose disconnects
+globals.mongoose.connection.on('disconnected', () => {
+  console.log('Mongoose disconnected');
+});
+
+//Disconnect mongoose if node process terminates and then log message
+process.on('SIGINT', function() {
+  globals.mongoose.connection.close(function () {
+    console.log('Mongoose connection disconnected through app termination');
+    process.exit(0);
+  });
 });
